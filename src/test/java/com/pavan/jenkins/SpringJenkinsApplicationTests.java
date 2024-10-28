12b937793066457ba24e65834b4c679d056a1735
pavan reddy <pavanreddykoduri@webster.edu>
2024-10-28 13:37:46 -0500

    second commit

--- src/test/java/com/pavan/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/pavan/jenkins/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing ");
+		logger.info("Test case executing secong log statement ");
 
 		assertEquals(true, true);
 	}
